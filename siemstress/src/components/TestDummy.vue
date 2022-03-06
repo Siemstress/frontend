533a7fd2e70f031f490fbd3727da747d315c7a1e
Zach Kroesen <zach@zachkroesen.com>
2022-03-06 04:59:32 -0500

    Fixed project compilation

--- siemstress/src/components/TestDummy.vue
+++ siemstress/src/components/TestDummy.vue
@@ -2,17 +2,22 @@
   <Html2PDFDemo :incidentData="this.data" :agentID="1"></Html2PDFDemo>
 </template>
 <script>
-import Html2PDFDemo from './Html2PDFDemo.vue'
+
+import Html2PDFDemo from "./reports/Html2PDFDemo";
+
 export default {
   name: "TestDummy",
   data: function () {
     return {
-        data: [{'code': "RUS", 'incidentCount': 20}, {'code': "USA", 'incidentCount': 14}, {'code': "CAN", 'incidentCount': 11}]
+      data: [{'code': "RUS", 'incidentCount': 20}, {'code': "USA", 'incidentCount': 14}, {
+        'code': "CAN",
+        'incidentCount': 11
+      }]
 
     }
   },
   components: {
-      Html2PDFDemo
+    Html2PDFDemo
   }
 };
 </script>
